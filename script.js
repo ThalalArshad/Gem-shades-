fec8eb4122bbdaabf1c0fca6493340c488e89b58
ThalalArshad <thalalarshad503@gmail.com>
2024-10-18 14:33:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript to toggle the menu on mobile
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+});
